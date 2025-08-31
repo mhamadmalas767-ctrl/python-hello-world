c4d045037231663cac3ac8f341869bab968a3a3b
mhamadmalas767-ctrl <mhamadmalas767@gmail.com>
2025-08-31 22:22:29 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,29 @@
-from http.server import BaseHTTPRequestHandler
+import telebot
+from flask import Flask, request
 
-class handler(BaseHTTPRequestHandler):
+TOKEN = "8146126501:AAFvVgU26HQNKSf2TLXISJhBigCLsqpKadI"
+bot = telebot.TeleBot(TOKEN)
+app = Flask(__name__)
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        return
+# الرد على /start
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "كيفك؟ 😎")
+
+# Webhook
+@app.route(f"/{TOKEN}", methods=['POST'])
+def webhook():
+    json_str = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return "!", 200
+
+# صفحة فارغة لتأكيد تشغيل السيرفر
+@app.route("/")
+def index():
+    return "بوت شغال على Vercel", 200
+
+if __name__ == "__main__":
+    bot.remove_webhook()
+    bot.set_webhook(url=f"https://اسم-مشروعك.vercel.app/{TOKEN}")
+    app.run()
